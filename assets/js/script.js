bf0cfd9f6eba329d93999eddf560fa49d916cb7a
saveetha17 <saveethakathiravan@gmail.com>
2023-07-11 18:33:59 -0400

    Worked on storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,13 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
     var city = document.getElementById("cityname").value;
+    var storeCity = JSON.parse(localStorage.getItem("WeatherDashboard")) || [];
+    storeCity.push(city);
+    localStorage.setItem("WeatherDashboard", JSON.stringify(storeCity));
+    document.getElementById("cityname").value = "";
     currentForecast(city);
     fiveDaysForecast(city);
+    storecityName();
   });
 
 var API = "4c22e0710598a7b61533045d9ad30012";
@@ -40,19 +45,6 @@ function currentForecast(city) {
       document.getElementById("todaysWeather").innerHTML = "";
       document.getElementById("todaysWeather").append(articleEle);
     });
-}
-function rendercityName() {
-  cityList.innerHTML = "";
-  cityCountSpan.textContent = cityName.length;
-
-  for (var i = 0; i < cityName.length; i++) {
-    var city = cityName[i];
-
-    var li = document.createElement("li");
-    li.textContent = city;
-    li.setAttribute("data-index", i);
-    cityList.appendChild(li);
-  }
 }
 
 //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
@@ -105,58 +97,21 @@ function fiveDaysForecast(city) {
     });
 }
 
-// function storecityName() {
+function storecityName() {
+  var storeCity = JSON.parse(localStorage.getItem("WeatherDashboard")) || [];
+  var history = "";
+  for (let i = 0; i < storeCity.length; i++) {
+    history += `<button class="btn btn-secondary history" type="button">${storeCity[i]}</button>`;
+  }
+  document.getElementById("city-names").innerHTML = history;
+  var searchCityList = document.querySelectorAll(".history");
+  searchCityList.forEach((city) => city.addEventListener("click", getWeather));
+}
 
-//     localStorage.setItem("cityName", JSON.stringify(cityname));
-// }
+storecityName();
 
-// storecityName();
-//  rendercityName();
-
-// function showtodaysWeather(city) {
-//     var cityHTML = `
-//     <div class="card">
-//             <div class="card-body">
-//             <h2 class="card-title">${city.name}</h2> <br>
-//             <p class="card-text">${city.humidity}</p>
-//             <p class="card-text">${city.temp}</p>
-//             <p class="card-text">${city.speed}</p>
-//         </div>
-//     </div>
-//     `
-
-//     $("#todaysWeather").html(cityHTML)
-// }
-
-// function shownextFiveDays(name) {
-//     var nextFiveDays = [];
-
-//     while (nextFiveDays.length < 5) {
-//         var randomIndex = Math.floor(Math.random() * alldays.length);
-//         var city = alldays[randomIndex];
-
-//         if (city.cityName !== name) {
-//             nextFiveDays.push(city)
-//         }
-//     }
-
-//     $("#nextFiveDays").empty();
-
-//     for (let i = 0; i < nextFiveDays.length; i++) {
-//         const friend = nextFiveDays[i];
-
-//         var cityCard = $(`
-//        <div class="col"
-//        <div class="card">
-//        <div class="card-body">
-//        <h2 class="card-title">${city.cityName}</h2> <br>
-//        <p class="card-text">${city.humidity}</p>
-//        <p class="card-text">${city.temp}</p>
-//        <p class="card-text">${city.speed}</p>
-//        </div>
-//        </div>
-//  `)
-
-//         $("#nextFiveDays").append(cityCard)
-//     }
-// }
+function getWeather(event) {
+  var cityName = event.target.textContent;
+  fiveDaysForecast(cityName);
+  currentForecast(cityName);
+}
